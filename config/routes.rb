f2f0dd0004902c2e70df5d171fbafc8e759af9a1
Claire Antoine <cantoine85@gmail.com>
2015-08-18 14:55:15 +0200

    transform restaurant inot task & add action index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get "tasks" => "tasks#index"
+
+  get "tasks/:id" => "tasks#show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
