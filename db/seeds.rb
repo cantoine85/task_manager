b5b7807c9d2b022f96bb452877dea9794bb801e7
Claire Antoine <cantoine85@gmail.com>
2015-08-18 14:52:38 +0200

    filling database with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+Task.create(name:"Faire la vaisselle", done:false)
+Task.create(name:"Acheter du pain", done:true)
+Task.create(name:"Apprendre un texte par coeur", done:false)
+Task.create(name:"Faire la révision de la voiture", done:false)
+Task.create(name:"Préparer un gateau", done:false)
+Task.create(name:"Faire mes comptes", done:false)
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
